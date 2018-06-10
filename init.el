d9d9ac3912e3202cfd752b0c1f1bf0f2780bdc55
propet <luisdomingoaranda@gmail.com>
2018-06-10 21:22:29 +0200

    evil plugins and electric-pair-mode

--- init.el
+++ init.el
@@ -5,9 +5,11 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
 
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 
 ; Themes to download
 (unless (package-installed-p 'nimbus-theme)
@@ -20,22 +22,39 @@
   (package-refresh-contents)
   (package-install 'oceanic-theme))
 
-; Packages to download
+
 (use-package which-key
   :ensure t
   :init
   (which-key-mode))
 
-; Give us back Ctrl+U for vim emulation
-(setq evil-want-C-u-scroll t)
-; Use evil mode
-(require 'evil)
-(evil-mode t)
+
+; load evil and extensions
+(use-package evil
+  :ensure t ;; install the evil package if not installed
+  :init ;; tweak evil's configuration before loading it
+  (setq evil-want-C-u-scroll t)
+  :config ;; tweak evil after loading it
+  (evil-mode 1)
+  ;; example how to map a command in normal mode (called 'normal state' in evil)
+  (define-key evil-normal-state-map (kbd ", w") 'evil-window-vsplit))
+
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :ensure t
+  :init
+  (evil-commentary-mode))
+
 
 ; Appearance
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(electric-pair-mode 1)
 
 
 (custom-set-variables
@@ -53,7 +72,7 @@
     ("4486ade2acbf630e78658cd6235a5c6801090c2694469a2a2b4b0e12227a64b9" "876fe28b6263ef36feeff1fa10db90a08ed899d1c6505b1d040d556db8ed0d2d" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
  '(package-selected-packages
    (quote
-    (oceanic-theme nimbus-theme spacemacs-theme which-key use-package evil-visual-mark-mode))))
+    (evil-commentary evil-surround oceanic-theme nimbus-theme spacemacs-theme which-key use-package evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
