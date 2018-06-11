72fe6860db13ae4dce6078ea2f14a4664435e53a
propet <luisdomingoaranda@gmail.com>
2018-06-11 18:00:34 +0200

    C/C++ autocompletion using company and irony, company-irony

--- init.el
+++ init.el
@@ -35,7 +35,7 @@
     ("4486ade2acbf630e78658cd6235a5c6801090c2694469a2a2b4b0e12227a64b9" "876fe28b6263ef36feeff1fa10db90a08ed899d1c6505b1d040d556db8ed0d2d" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default)))
  '(package-selected-packages
    (quote
-    (spaceline smex ido-vertical-mode org-bullets beacon nlinum-relative evil-commentary evil-surround oceanic-theme nimbus-theme spacemacs-theme which-key use-package evil-visual-mark-mode))))
+    (company-irony company spaceline smex ido-vertical-mode org-bullets beacon nlinum-relative evil-commentary evil-surround oceanic-theme nimbus-theme spacemacs-theme which-key use-package evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
